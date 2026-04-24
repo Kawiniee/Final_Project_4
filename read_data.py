4378825b1efddfa2ae38406dd1fc608de5669261
kawin <kamkaewkawin@gmail.com>
2026-04-24 19:05:53 +0700

    new version

--- read_data.py
+++ read_data.py
@@ -2,14 +2,14 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 
 data = pd.read_csv('RTD_Brew.csv')
-segmented = pd.read_csv('unsupervised_results.csv')
+# segmented = pd.read_csv('unsupervised_results.csv')
 
 print("----- Check Raw Data -----")
 print(data.info())
 print("----- Check Unsupervised -----")
-print(segmented.info())
+# print(segmented.info())
 
-def data_prep(data, segmented):
+def data_prep(data):
 # for unsupervised
     mask = (data['คุณดื่มกาแฟหรือไม่'] == 'ไม่ดื่ม') & (data['คุณดื่มชาหรือไม่'] == 'ไม่ดื่ม')
     data = data.drop(data[mask].index).reset_index() #drop where it's not ours customer
@@ -29,6 +29,7 @@ def data_prep(data, segmented):
                 data[col].astype(str).str.replace(r'\D', '', regex=True), 
                 errors='coerce'
         )
+    
     # Rename columns before returning
     # Uncomment and add column names here to rename them
     rename_mapping = {
@@ -62,74 +63,80 @@ def data_prep(data, segmented):
     }
 
     data = data.rename(columns=rename_mapping)
-    cleaned_data = data[list(rename_mapping.values())].copy()
+    data = data[list(rename_mapping.values())].copy()
+
+    data['C_Occasion'] = data['C_Occasion'].fillna('ไม่ดื่มกาแฟ')
+    data['T_Occasion'] = data['T_Occasion'].fillna('ไม่ดื่มชา')
+    data = data.fillna(0)
+    print('----- Check missing data -----')
+    print(data.isna().sum())
 
 #for supervised
-    data["Cluster_ID"] = segmented["Cluster_ID"]
+#     data["Cluster_ID"] = segmented["Cluster_ID"]
 
-    #rename some more columns
-    data = data.rename(columns={'อายุ': 'Age', 'อาชีพ': 'Profession', 'เพศ': 'Sex',
-                                'โปรดพิมพ์จังหวัดที่อยู่อาศัยของคุณ เช่น กทม , ขอนแก่น, ชลบุรี': 'Province',
-                                'คุณดื่มกาแฟประเภทใดบ่อยที่สุด': 'C_Frequency',
-                                'คุณใช้โซเชียลมีเดียใดบ่อยที่สุด': 'S_Occasion',
-                                'ความถี่ในการเปิดรับสื่อในแต่ละช่องทางต่อสัปดาห์ [ออนไลน์]': 'S_Frequency',
-                                'ระยะเวลาในการเสพสื่อต่อวัน ในแต่ละช่องทาง [ออนไลน์]': 'S_Screentime',
-                                'ในวันจันทร์-ศุกร์ (Weekday) ช่วงเวลาใดที่คุณเปิดรับสื่อแต่ละช่องทาง [ออนไลน์]': 'S_Time(weekday)',
-                                'ในวันหยุดเสาร์-อาทิตย์ (Weekend) ช่วงเวลาใดที่คุณเปิดรับสื่อแต่ละช่องทาง [ออนไลน์]': 'S_Time(weekend)',
-                                'ในวันหยุดยาวหรือเทศกาล คุณใช้โซเชียลมีเดีย อย่างไร': 'S_Usage(festival)'})
+#     #rename some more columns
+#     data = data.rename(columns={'อายุ': 'Age', 'อาชีพ': 'Profession', 'เพศ': 'Sex',
+#                                 'โปรดพิมพ์จังหวัดที่อยู่อาศัยของคุณ เช่น กทม , ขอนแก่น, ชลบุรี': 'Province',
+#                                 'คุณดื่มกาแฟประเภทใดบ่อยที่สุด': 'C_Frequency',
+#                                 'คุณใช้โซเชียลมีเดียใดบ่อยที่สุด': 'S_Occasion',
+#                                 'ความถี่ในการเปิดรับสื่อในแต่ละช่องทางต่อสัปดาห์ [ออนไลน์]': 'S_Frequency',
+#                                 'ระยะเวลาในการเสพสื่อต่อวัน ในแต่ละช่องทาง [ออนไลน์]': 'S_Screentime',
+#                                 'ในวันจันทร์-ศุกร์ (Weekday) ช่วงเวลาใดที่คุณเปิดรับสื่อแต่ละช่องทาง [ออนไลน์]': 'S_Time(weekday)',
+#                                 'ในวันหยุดเสาร์-อาทิตย์ (Weekend) ช่วงเวลาใดที่คุณเปิดรับสื่อแต่ละช่องทาง [ออนไลน์]': 'S_Time(weekend)',
+#                                 'ในวันหยุดยาวหรือเทศกาล คุณใช้โซเชียลมีเดีย อย่างไร': 'S_Usage(festival)'})
     
-    features = data[['Age', 'Profession', 'Sex',
-        'Province',
-        'S_Occasion',
-        'C_Occasion',
-        'C_Frequency',
-        'S_Frequency',       
-        'S_Screentime',
-        'S_Time(weekday)',
-        'S_Time(weekend)',
-        'S_Usage(festival)', 'Cluster_ID']].copy()
+#     features = data[['Age', 'Profession', 'Sex',
+#         'Province',
+#         'S_Occasion',
+#         'C_Occasion',
+#         'C_Frequency',
+#         'S_Frequency',       
+#         'S_Screentime',
+#         'S_Time(weekday)',
+#         'S_Time(weekend)',
+#         'S_Usage(festival)', 'Cluster_ID']].copy()
     
-    mapping = {
-        'กทม': 'กรุงเทพมหานคร',
-        'กทม.': 'กรุงเทพมหานคร',
-        'กรุงเทพ': 'กรุงเทพมหานคร',
-        'กมม': 'กรุงเทพมหานคร',
-        'bkk': 'กรุงเทพมหานคร',
-        'bangkok': 'กรุงเทพมหานคร',
-        'dกทม': 'กรุงเทพมหานคร',
-        'แอลเอ': 'ร้อยเอ็ด',
-        'bonn': 'อุบลราชธานี'
-    }
+#     mapping = {
+#         'กทม': 'กรุงเทพมหานคร',
+#         'กทม.': 'กรุงเทพมหานคร',
+#         'กรุงเทพ': 'กรุงเทพมหานคร',
+#         'กมม': 'กรุงเทพมหานคร',
+#         'bkk': 'กรุงเทพมหานคร',
+#         'bangkok': 'กรุงเทพมหานคร',
+#         'dกทม': 'กรุงเทพมหานคร',
+#         'แอลเอ': 'ร้อยเอ็ด',
+#         'bonn': 'อุบลราชธานี'
+#     }
 
-    #Keep first element drop the rest
-    features['Province'] = features['Province'].str.split(',').str[0].str.strip().str.lower()
+#     #Keep first element drop the rest
+#     features['Province'] = features['Province'].str.split(',').str[0].str.strip().str.lower()
 
-    #Mapping
-    features['Province'] = features['Province'].map(mapping).fillna(features['Province'])
-    features['C_Frequency'] = features['C_Frequency'].fillna('ไม่ดื่มกาแฟประเภทใดเลย')
+#     #Mapping
+#     features['Province'] = features['Province'].map(mapping).fillna(features['Province'])
+#     features['C_Frequency'] = features['C_Frequency'].fillna('ไม่ดื่มกาแฟประเภทใดเลย')
 
-    # Separating columns for Different Encodings
-    features_binary = ['Profession', 'Sex', 'S_Usage(festival)', 'Province', 'C_Occasion', 'C_Frequency', 'Cluster_ID']
-    features_label = ['Age' ,'S_Frequency', 'S_Time(weekday)', 'S_Time(weekend)', 'S_Screentime']
+#     # Separating columns for Different Encodings
+#     features_binary = ['Profession', 'Sex', 'S_Usage(festival)', 'Province', 'C_Occasion', 'C_Frequency', 'Cluster_ID']
+#     features_label = ['Age' ,'S_Frequency', 'S_Time(weekday)', 'S_Time(weekend)', 'S_Screentime']
 
-    # OneHot (Binary) Encode
-    ohe = OneHotEncoder(sparse_output=False)
-    features_cols_binary = ohe.fit_transform(features[features_binary].astype(str))
-    features_cols_binary_df = pd.DataFrame(features_cols_binary, columns=ohe.get_feature_names_out(features_binary))
+#     # OneHot (Binary) Encode
+#     ohe = OneHotEncoder(sparse_output=False)
+#     features_cols_binary = ohe.fit_transform(features[features_binary].astype(str))
+#     features_cols_binary_df = pd.DataFrame(features_cols_binary, columns=ohe.get_feature_names_out(features_binary))
 
-    # Label Encode
-    le = LabelEncoder()
-    features_cols_label_df = features[features_label].copy().reset_index(drop=True)
-    for col in features_label:
-        features_cols_label_df[col] = le.fit_transform(features_cols_label_df[col].astype(str))
+#     # Label Encode
+#     le = LabelEncoder()
+#     features_cols_label_df = features[features_label].copy().reset_index(drop=True)
+#     for col in features_label:
+#         features_cols_label_df[col] = le.fit_transform(features_cols_label_df[col].astype(str))
     
-    # Combine back into period_cols
-    features = pd.concat([features_cols_binary_df, features_cols_label_df], axis=1)
-    print("----- Check Encoded features -----")
-    print(features)
+#     # Combine back into period_cols
+#     features = pd.concat([features_cols_binary_df, features_cols_label_df], axis=1)
+#     print("----- Check Encoded features -----")
+#     print(features)
 
-    return features, cleaned_data
+    return data
 
-features, cleaned_data = data_prep(data, segmented)
-features.to_csv('for_supervised.csv', index=False)
-cleaned_data.to_csv('for_unsupervised.csv', index=False)
+data = data_prep(data)
+# features.to_csv('for_supervised.csv', index=False)
+data.to_csv('for_unsupervised.csv', index=False)
