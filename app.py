6a3615452eb3a1dba4f8f4d98b965841418fead1
Kawin <156191795+Kawiniee@users.noreply.github.com>
2026-05-12 16:58:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ app = dash.Dash(
     suppress_callback_exceptions=True,
 )
 
+server = app.server
+
 navbar = dbc.NavbarSimple(
     children=[
         dbc.NavItem(dbc.NavLink("🏠 Home", href="/", active="exact")),
